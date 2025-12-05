06f60c1cdaf5e113961c9b70cab2ce27ef6a6bc3
Ta-Lo <tasalopareva@gmail.com>
2025-12-05 21:49:38 +0700

    Add files via upload

---
+++ Lopareva/lab_9.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	float x;
+	float y;
+	float z;
+
+	cout << "x = " << endl;
+	cin >> x;
+
+	cout << "y = " << endl;
+	cin >> y;
+
+	z = (x*x - y*y + 2 * abs(x * y)) / (1 + x*x + y *y);
+
+	cout << "z = "<< z<<endl;
+	
+	
+
+
+
+}
