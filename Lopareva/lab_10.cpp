1193a3c1c9e9e5dbc062640009a8c8f53b0c4008
Ta-Lo <tasalopareva@gmail.com>
2025-12-05 22:12:14 +0700

    Add files via upload

---
+++ Lopareva/lab_10.cpp
@@ -0,0 +1,33 @@
+﻿//ВАРИАНТ 1
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	float x;
+	float y;
+	float z;
+
+	cout <<"x = ";
+	cin >> x;
+
+	cout << "y = ";
+	cin >> y;
+
+	if (x <= 0) {
+		
+		z = pow(pow(x, 2) + pow(y, 2), 1 / 3);
+	}
+	if (x > 0 && abs(y) >= 0) 
+	{
+	
+		z = asin(x / y);
+	}
+	if (x > 0 && abs(y) < 0) {
+	
+		z = log(x + y) / log(5);
+	}
+	cout << "z = " << z; 
+
+}
