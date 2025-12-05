00c285e99a36044c4923364cbd5d6195398fb7a6
Ta-Lo <tasalopareva@gmail.com>
2025-12-05 22:36:20 +0700

    Add files via upload

---
+++ Lopareva/lab_11.cpp
@@ -0,0 +1,27 @@
+﻿//ВАРИАНТ 1
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+    double x;
+
+    cout << "n: ";
+    cin >> n;
+    cout << "x: ";
+    cin >> x;
+
+    double Q = 0.0, S = 0.0;
+
+    for (int k = 1; k <= n; ++k) {
+        Q = (k % 2 == 1 ? 1 : -1) * pow(x, k) / k; //(условие ? значение_если_истина : значение_если_ложь)
+        S += Q;
+    }
+
+    cout << "Q = " << Q << endl;
+    cout << "S = " << S << endl;
+
+    return 0;
+}
