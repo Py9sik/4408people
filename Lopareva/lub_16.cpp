8700b7cc46edb5c0c2e2dafb8f09c0f73c8cdd00
Ta-Lo <tasalopareva@gmail.com>
2025-12-20 14:35:41 +0700

    Add files via upload

---
+++ Lopareva/lub_16.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Recursia for Fibanacci's number
+
+template<typename T>
+T fib( T n) {
+    if (n <= 2)
+        return static_cast<T>(n);
+    return fib(static_cast<T>(n - 1)) + fib(static_cast<T>(n - 2));
+}
+
+int main() {
+    int k;
+
+    cout << "Enter k: ";
+    cin >> k;
+
+  
+    if (k < 2) {
+        cout << "ERROR" << endl;
+        return 1;
+    }
+
+    auto res = fib<float>(static_cast<float> (k));
+
+    cout << "Fibanacci(" << k << ") = " << res << endl;
+   
+    return 0;
+}
