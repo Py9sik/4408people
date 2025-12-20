1ae6375dc1eb3923319d4a215a9a660734a6aefb
Ta-Lo <tasalopareva@gmail.com>
2025-12-20 14:35:06 +0700

    Add files via upload

---
+++ Lopareva/13.12.25.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a[12];
+
+	for (int i = 0; i < 12; i++) {//fulling massive
+		a[i] = i + 1;
+	}
+	cout << "Beginning: ";
+	for (int i = 0; i < 12; i++) {
+		cout << a[i] << " ";
+	}
+	for (int i = 0; i < 11; i += 2) { //switch number + and -
+		int t = a[i];
+		a[i] = a[i + 1]; //in i wrighting next meaning i+1
+		a[i + 1] = t;//wright saving meaning
+	}
+	cout << "Became: ";
+	for (int i = 0; i < 12; i++) {
+		cout << a[i] << " ";
+	}
+}
---
+++ Lopareva/20.12.25.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int delEl(int a[], int s, int ind) {
+
+	if (ind < 0 || ind >= s) {
+		
+		cout << "Error: index uotside massive" << endl;
+		return s;
+	
+	}
+	for (int i = ind; i < s-1; i++) { //slide all element on one position in left
+		
+		a[i] = a[i + 1];
+	
+	}
+
+	return s - 1; // return new size of massive
+}
+
+int main() {
+	int s;
+	const int m = 100;
+	int a[100];
+	int ind;
+
+	cout << "Enter size of massive: "<<endl;
+	cin >> s;
+
+	cout << "Enter " << s << " of massive: "<<endl; // enter elemets of massive
+	for (int i = 0; i < s; i++) {
+		cin >> a[i];
+	}
+
+	cout << "First massive: ";
+	for (int i = 0; i < s; i++) {
+		
+		cout << a[i]<<" ";
+	}
+	
+
+	cout << "Enter index of element for delete: ";
+	cin >> ind;
+
+	int ns = delEl(a, s, ind); //delete element and creat new size
+
+	if (ns < s) {
+	
+		s = ns;
+		cout << "Second massive: ";
+		
+		for (int i = 0; i < s; i++) {
+			cout << a[i] << " ";
+		}
+		cout << endl;
+	}
+	else {
+		cout << "Elements not deleted, because not right index";
+	}
+	
+}
